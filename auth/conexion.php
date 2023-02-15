2417405fa0ab833dea0887a49ccb7b4b051da379
Fernando <valero.ce@gmail.com>
2023-02-15 23:51:32 +0100

    Prueba 4

--- auth/conexion.php
+++ auth/conexion.php
@@ -1,7 +1,7 @@
 <?php
 
-include "../config/dbconnect.php";
-include "../view/templates/head.php";
+include "/config/dbconnect.php";
+include "/view/templates/head.php";
 
 if (
   empty($_POST['user']) ||
@@ -13,35 +13,35 @@ if (
           <a href='/index.php' class='button'>Volver</a>
       </div>
 ";
-die();
+  die();
 }
 
-$user=$_POST['user'];
-$password=$_POST['password'];
+$user = $_POST['user'];
+$password = $_POST['password'];
 
+
+$db = new Db();
+$conection = $db->conection;
 $sql = "SELECT * FROM users WHERE user = '$user'";
-$resultado = $link->prepare($sql);
-$resultado->execute();
-$fila = $resultado->fetch();
+$query = $conection->prepare($sql);
+$query->execute();
+$fila = $query->fetchAll(PDO::FETCH_ASSOC);
 $passwordhash = $fila[2];
 $id = $fila[0];
 
 
 
 if (password_verify($password, $passwordhash)) {
-    $type = $fila[3];
-    session_start();
-    $_SESSION['id_user']=$id;
-    $_SESSION['name_user']=$user;
-    $_SESSION['type_user'] = $type;
-    $_SESSION['autentificado'] = "OK";
-    header ("Location: /view/home.php");
+  $type = $fila[3];
+  session_start();
+  $_SESSION['id_user'] = $id;
+  $_SESSION['name_user'] = $user;
+  $_SESSION['type_user'] = $type;
+  $_SESSION['autentificado'] = "OK";
+  header("Location: /view/home.php");
+} else {
+
+
+
+  header("Location: /view/login.php?mensage=Usuario o contraseña incorrecta");
 }
-else {
-
-
-  
-header ("Location: /view/login.php?mensage=Usuario o contraseña incorrecta");
-}
-
-?>
